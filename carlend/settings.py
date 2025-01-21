6d67be3c72ba1e18d16c30edb5b17ad05f571921
said.T <tarhisaid@gmail.com>
2025-01-21 01:33:44 +0100

    database url

--- carlend/settings.py
+++ carlend/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = "carlend.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL")),
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL","postgresql://postgres:HbsatcAhaSUtHTNBgMdWMaZVoTmBzhpO@postgres.railway.internal:5432/railway")),
 }
 
 
