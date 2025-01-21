d26f3c10b3bcfb44058a8d53dd2f102396bb8464
said.T <tarhisaid@gmail.com>
2025-01-21 20:25:00 +0100

    change to github repo

--- carlend/settings.py
+++ carlend/settings.py
@@ -97,8 +97,8 @@ WSGI_APPLICATION = "carlend.wsgi.application"
 # Set default values for the environment variables if they’re not already set
 os.environ.setdefault("PGDATABASE", "railway")
 os.environ.setdefault("PGUSER", "postgres")
-os.environ.setdefault("PGPASSWORD", "HbsatcAhaSUtHTNBgMdWMaZVoTmBzhpO")
-os.environ.setdefault("PGHOST", "junction.proxy.rlwy.net")
+os.environ.setdefault("PGPASSWORD", "bHTJFQfiYyobjUWKwPWltZENCTFNhhYa")
+os.environ.setdefault("PGHOST", "viaduct.proxy.rlwy.net")
 os.environ.setdefault("PGPORT", "5432")
 
 DATABASES = {
