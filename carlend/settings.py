29975cbe3b76c2774e13448a4a3c38dbc48786a7
said.T <tarhisaid@gmail.com>
2025-01-23 18:43:24 +0100

    Ajout de Robots.txt pour minimiser des Tasks inutiles

--- carlend/settings.py
+++ carlend/settings.py
@@ -82,8 +82,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = "carlend.urls"
 
 TEMPLATES = [
