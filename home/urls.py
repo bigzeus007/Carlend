63d32b143acba19a312b6559c838776cde0af576
said.T <tarhisaid@gmail.com>
2025-03-14 22:47:13 +0000

    correction home.urls

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,6 @@
-from django.urls import path, include  # Ajout de `include` pour inclure d'autres applications
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Vue principale du tableau de bord
-    path('reservations/', include('reservations.urls')),  # Inclure les URLs de l'application reservations
-    path('vehicles/', include('vehicles.urls')),  # Inclure les URLs de l'application vehicles
-    path('parc/', include('parc.urls')),  # Inclure les URLs de l'application parc (si besoin)
+    path('', views.home, name='home'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
